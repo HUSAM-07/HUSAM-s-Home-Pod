72586504318e389aa22799ed6425710eb18578e4
Mohammed Husamuddin <uni.husam@gmail.com>
2023-08-15 19:42:53 +0400

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,14 @@
 import streamlit as st
+from PIL import Image
 
 def main():
     st.title("Home WiFi QR Code Display")
 
     # Display QR Code image
-    qr_code_image = r"C:\Users\UNIHU\Desktop\CODE DIRECTORY\Home_Pod\HUSAM-s-Home-Pod\Wifi.jpg"  # Replace with the actual image path
-    st.image(qr_code_image, caption="WiFi QR Code", use_container_width=True)
+
+image = Image.open('Wifi.jpg')
+
+st.image(image, caption='Sunrise by the mountains')
 
 if __name__ == "__main__":
     main()
