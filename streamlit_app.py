988c5a75c06e66c952e05be539dfc10a155471ba
Mohammed Husamuddin <uni.husam@gmail.com>
2023-08-15 19:39:38 +0400

    Push

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ def main():
     st.title("Home WiFi QR Code Display")
 
     # Display QR Code image
-    qr_code_image = "C:\Users\UNIHU\Desktop\CODE DIRECTORY\Home_Pod\HUSAM-s-Home-Pod\Wifi.jpg"  # Replace with the actual image path
+    qr_code_image = r"C:\Users\UNIHU\Desktop\CODE DIRECTORY\Home_Pod\HUSAM-s-Home-Pod\Wifi.jpg"  # Replace with the actual image path
     st.image(qr_code_image, caption="WiFi QR Code", use_container_width=True)
 
 if __name__ == "__main__":
